8703869e68f14d7d00a6f57671898770fec3831f
tianshuzi <drunkwind@aliyun.com>
2020-05-05 15:43:42 +0800

    springboot 预定第一次提交

---
+++ src/main/java/com/booking/bookingboot/BookingBootApplication.java
@@ -0,0 +1,13 @@
+package com.booking.bookingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booking/bookingboot/BookingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.bookingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
